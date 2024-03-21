7dc9dc27441b53fc547be716b2c4c6fad0c683d6
sergi.moliner <sergilopezmoliner@gmail.com>
2024-03-21 13:26:19 +0100

    ex2

--- src/films.js
+++ src/films.js
@@ -6,7 +6,8 @@ function getAllDirectors(array) {
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
-
+  let moviesFound = array.filter(movie => movie.director === director);
+  return moviesFound;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
